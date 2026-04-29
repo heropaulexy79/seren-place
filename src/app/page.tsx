5bf9c9601603cb7d26b840d9319bf045744269f9
heropaulexy <okeoluwaseun79@gmail.com>
2026-04-29 13:39:51 +0100

    little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSlider from "@/components/home/HeroSlider";
 import TrustBadges from "@/components/home/TrustBadges";
 import AboutOverview from "@/components/home/AboutOverview";
 import ServicesOverview from "@/components/home/ServicesOverview";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
 import HowItWorks from "@/components/home/HowItWorks";
 import StatsSection from "@/components/home/StatsSection";
 import Testimonials from "@/components/home/Testimonials";
@@ -14,6 +15,7 @@ export default function Home() {
       <TrustBadges />
       <AboutOverview />
       <ServicesOverview />
+      <WhyChooseUs />
       <HowItWorks />
       <StatsSection />
       <Testimonials />
